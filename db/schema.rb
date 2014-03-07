0f9e59aa2cde863045e4bc8d888b506620933b70
Christian Stuff <chrisstuff-it.de>
2014-03-07 09:53:48 +0100

    fixed groups setup

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(:version => 20120725072510) do
     t.integer  "owner_id"
   end
 
+  create_table "groups", :force => true do |t|
+    t.string   "name"
+    t.integer  "leader_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
   create_table "groups_users", :force => true do |t|
     t.integer "group_id"
     t.integer "user_id"
